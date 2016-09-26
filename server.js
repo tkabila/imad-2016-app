750dd636496ad7d83c814bb27d97bfef0dda7db9
tkabila <tkabila@gmail.com>
2016-09-26 17:21:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
-}
+
 var articles = {
     
    `article-one`: {
