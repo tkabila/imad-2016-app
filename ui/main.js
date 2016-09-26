456aa53af3a8040cdebb5e84d79ee6998d0727c4
tkabila <tkabila@gmail.com>
2016-09-27 00:02:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'Super !!';
 var img = document.getElementById('madi');
-img.onClick = function(){
+img.onclick = function(){
   img.style.marginLeft ='100px';  
     
 };
