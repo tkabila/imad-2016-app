1e106ee116a2bc83ceaf30a219aa122ea552f927
tkabila <tkabila@gmail.com>
2016-09-29 01:46:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@
 var button = document.getElementById('counter');
 
 button.onclick = function(){
-    var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
    
     request.onreadystatechange = function(){
      if(request.readyState===XMLHttpRequest.DONE){
@@ -13,22 +13,36 @@ button.onclick = function(){
              span.innerHTML = counter.toString();
          }
      }    
-        
+    
     };
     request.open('GET','http://tkabila.imad.hasura-app.io/counter',true);
     request.send(null);
 };
  
- var nameInput = document.getElementById('name');
- var name = nameInput.value;
+
  var submit = document.getElementById('submit_btn');
  submit.onclick = function(){
-     var names = ['name1','name2','name3','name4'];
-     var list = ' ';
-     for(var i=0;i<names.length;i++){
+     var request = new XMLHttpRequest();
+   
+    request.onreadystatechange = function(){
+     if(request.readyState===XMLHttpRequest.DONE){
+         if(request.status === 200){
+             var names = request.responseText;
+             names = JSON.parse(names);
+              var list = ' ';
+        for(var i=0;i<names.length;i++){
          list+='<li>' +names[i]+'</li>' ;
      }
      var ul = document.getElementById('namelist');
      ul.innerHTML = list;
+         }
+     }    
+    
+    };
+     var nameInput = document.getElementById('name');
+     var name = nameInput.value;
+    request.open('GET','http://tkabila.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+     ;
      
  };
