a3b7590b8b825f621bb9666ccb7e708104fccfd2
tkabila <tkabila@gmail.com>
2016-09-28 19:26:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var buttom = document.getElementById("counter");
 var counter =0;
 button.onclick = function(){
     counter =counter+1;
-    var span = document.getElementById('count');
+    var span = document.getElementById("count");
     span.innerHTML = counter.toString();
 };
