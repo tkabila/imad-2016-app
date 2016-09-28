88c786c23eebd01e4ae04d9b479d842beaa9286a
tkabila <tkabila@gmail.com>
2016-09-28 17:21:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get(`/counter`, function(req,res){
     counter= counter+1;
-    res.send(counter.toString());
+    res.send(counter.toString(8));
 
 });
 app.get(`/:articleName`,function(req,res){
