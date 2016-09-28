648135d0566799dc06e83315c17c518daf536041
tkabila <tkabila@gmail.com>
2016-09-28 21:14:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ button.onclick = function(){
          if(request.status === 200){
              var counter = request.responseText;
              var span = document.getElementById('count');
-             span.innerHTML = counte.toString();
+             span.innerHTML = counter.toString();
          }
      }    
         
