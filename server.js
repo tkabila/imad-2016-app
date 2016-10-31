ebff75f90a7bb5c0633b9cb67b464a463a5bd059
tkabila <tkabila@gmail.com>
2016-10-31 18:35:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ var config = {
     port : '5432',
     password : process.env.DB_PASSWORD
   };  
-
 var app = express();
 app.use(morgan('combined'));
-
 var articles = {
     'article-one':{
         title :`Article One|tkabila`,
