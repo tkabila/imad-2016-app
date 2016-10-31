2dca1b6ef540046c9ab89e76054d47289c2a8acc
tkabila <tkabila@gmail.com>
2016-10-31 15:15:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool = new Pool(config);
-app.get(`/test-db`,function(req,res){
+app.get(`/test-db`, function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if (err)
         {
