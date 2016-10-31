700a29d0b35a4a489d200037ad0b92995fd214b0
tkabila <tkabila@gmail.com>
2016-10-31 14:36:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 var config = {
     user : `tkabila`,
     database : `tkabila`,
     host : `db.imad.hasura.app.io,
     port : 5432,
     password : process.env.DB_PASSWORD
-    
+  }  
 
 var app = express();
 app.use(morgan('combined'));
@@ -94,7 +94,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new pool(config)
+var pool = new Pool(config)
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result)){
         if (err)
