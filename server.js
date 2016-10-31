36a144f4c90e71bc11c35b9ea6afd2e8bb1a2284
tkabila <tkabila@gmail.com>
2016-10-31 22:00:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 });
 app.get(`/articles/:articleName`,function(req,res){
-    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName +"'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
