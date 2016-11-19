e1f5fd3e2830093a9636ddceaeda4b27738fa615
tkabila <tkabila@gmail.com>
2016-11-20 01:20:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/create-user', function(req,res){
             res.status(500).send(err.toString());
                 }
                 else{
-                    res.send('user suceessfully created:'+username);
+                    res.send('user suceessfully created:' +username);
                 }
         
     });
