ea21f89108776ab3459498df1bdc49099124250b
tkabila <tkabila@gmail.com>
2016-11-19 09:00:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user : 'tkabila',
     database : 'tkabila',
@@ -53,6 +54,14 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+function hash(input,salt){
+    var hashed = crypto.pbkdf25Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.param.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 var pool = new Pool(config);
 app.get('/test-db', function(req,res){
