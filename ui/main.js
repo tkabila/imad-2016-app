53a36ab68a64d560fcee7958d58b199bb72ef5c4
tkabila <tkabila@gmail.com>
2016-11-20 18:23:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@
      var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST','http://tkabila.imad.hasura-app.io/login,true);
+    request.open('POST','http://tkabila.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');             
     request.send(JSON.stringify({username:username,password:password}));
       };
