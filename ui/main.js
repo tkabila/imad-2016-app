fb94e7f558b6b66f898885656dd905406f432cde
tkabila <tkabila@gmail.com>
2016-11-20 17:16:13 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,4 @@
-//console.log('Loaded!');
-//
-var button = document.getElementById('counter');
 
-button.onclick = function(){
-var request = new XMLHttpRequest();
-   
-    request.onreadystatechange = function(){
-     if(request.readyState===XMLHttpRequest.DONE){
-         if(request.status === 200){
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-             span.innerHTML = counter.toString();
-         }
-     }    
-    
-    };
-    request.open('GET','http://tkabila.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
- 
 
  var submit = document.getElementById('submit_btn');
  submit.onclick = function(){
@@ -27,22 +7,21 @@ var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
      if(request.readyState===XMLHttpRequest.DONE){
          if(request.status === 200){
-             var names = request.responseText;
-             names = JSON.parse(names);
-              var list = ' ';
-        for(var i=0;i<names.length;i++){
-         list+='<li>' +names[i]+'</li>' ;
+           alert('Logged  in successfully');
+          }
+       else if(request.status === 403){
+           alert('username/password is incorrect');
+          }
+        else if(request.status === 500){
+           alert('Something went wrong on the server');
+          }
      }
-     var ul = document.getElementById('namelist');
-     ul.innerHTML = list;
-         }
-     }    
-    
-    };
-     var nameInput = document.getElementById('name');
-     var name = nameInput.value;
-    request.open('GET','http://tkabila.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
-     ;
-     
- };
+     };
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://tkabila.imad.hasura-app.io/login,true);
+    request.setRequestHeader('Content-Type','application/json');             
+    request.send(JSON.stringify({username:username,password:password}));
+      };
