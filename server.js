1c649bde530713be8ef65299c9f1591ff5a2f5af
tkabila <tkabila@gmail.com>
2016-11-20 21:29:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require(express-session);
 var config = {
     user : 'tkabila',
     database : 'tkabila',
@@ -14,6 +15,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret : 'someRandomSecretValue',
+    cookie : {maxAge : 1000*60*60*24*30}
+}));
 function createTemplate(data){
     var title = data.title;
     var date = data.date;
@@ -96,6 +101,7 @@ app.post('/login', function(req,res){
                     var salt = dbString.split('$')[2];
                     var hashedPassword = hash(password,salt);
                     if(hashedPassword === dbString){
+                        req.session.auth = {userId: result.rows[0].id};
                         res.send('credentials correct!');
                     }
                     else{
@@ -105,6 +111,14 @@ app.post('/login', function(req,res){
                 }
         
     });
+});
+app.get('check-login',function(req,res){
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('you are logged in:'+req.session.auth.userId.toString());
+    }
+    else{
+        res.send('You are logged in');
+    }
 });
 var pool = new Pool(config);
 app.get('/test-db', function(req,res){
