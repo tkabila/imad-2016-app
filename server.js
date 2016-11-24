c0a72d010c6d1f7cb099ac006c66601cd3c5bb06
tkabila <tkabila@gmail.com>
2016-11-25 00:31:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,20 +133,6 @@ app.get('/logout', function(req,res){
     res.send('Loggedout');
 });
 var pool = new Pool(config);
-app.get('/test-db', function(req,res){
-    pool.query(`SELECT * FROM test`, function(err,result){
-        if (err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else{
-            res.send(JSON.stringify(result.rows));
-        }
-    });
-    
-});
-
- 
 app.get(`/articles/:articleName`,function(req,res){
     pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function(err,result){
         if(err){
