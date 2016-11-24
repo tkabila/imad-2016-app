0bacfc523545a71c96f1db87cba377e577bdafbf
tkabila <tkabila@gmail.com>
2016-11-24 19:56:41 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -2,7 +2,9 @@ function loadCommentForm(){
 var commentFormHtml = `
 <h4>Submit your valuable comment... </h4>
 <text area id = "comment-text" rows = "10" cols = "50"></text area>
+<br/>
 <input type = "submit" id = "submit" value = "submit"/>
+<br/>
 `;
 }
 document.getElementById('comment-form').innerHTML = commentFormHtml;
