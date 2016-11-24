cefd5a9788c41f91cb70dc2771401999693d883f
tkabila <tkabila@gmail.com>
2016-11-24 19:17:19 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,6 +1,6 @@
 function loadCommentForm(){
 var commentFormHtml = `
 <h4>Submit your valuable comment... </h4>
-<text area id = "comment_text" rows = "10" cols = "100"></text area>
+<text area id = "comment-text" rows = "10" cols = "100"></text area>
 `;
 }
