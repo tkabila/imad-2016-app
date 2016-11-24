9589797796753cc092eb075d265ba406e3ad8a51
tkabila <tkabila@gmail.com>
2016-11-24 20:55:59 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -6,5 +6,26 @@ var commentFormHtml = `
 <input type = "submit" id = "submit" value = "submit"/>
 <br/>
 `;
-}
 document.getElementById('comment-form').innerHTML = commentFormHtml;
+var submit = document.getElementById("submit");
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.Done){
+            if(request.status === 200){
+                document.getElementById('comment-text').value=' ';
+                loadcomments();
+            }
+            else{
+                alert('Error! Could not submit comment');
+            }
+            submit.value = 'submit';
+        }
+    };
+   var comment = document.getElementById('comment-text').value;
+   request.open('POST','/submit-comment' + currentArticleTitle,true);
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({comment: comment}));
+   submit.value = 'Submitting...';
+};
+}
