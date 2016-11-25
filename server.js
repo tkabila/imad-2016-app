77c1c303a46f58541c022c7ad62aa6010fafd165
tkabila <tkabila@gmail.com>
2016-11-25 23:18:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -206,12 +206,12 @@ app.get('/logout', function (req, res) {
     });
     });
 
-/*app.get('/ui/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/article.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.js'));
-});*/
+});
 
 app.get('/ui/fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
