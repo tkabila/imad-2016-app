ac89cefca19f61cb247c383ccf4d27ef9d5e97e7
tkabila <tkabila@gmail.com>
2016-11-25 13:20:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -191,10 +191,10 @@ var pool = new Pool(config);
     });
     });
 
-/*app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+/*
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
