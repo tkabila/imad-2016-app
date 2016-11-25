2bac4af0f7278c21782e7cd292532b233f2cb8e5
tkabila <tkabila@gmail.com>
2016-11-25 23:26:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -212,6 +212,9 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/article.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.js'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 app.get('/ui/fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
