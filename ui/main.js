bfa2a100dfc996c0d269045db2a5913d279d25e7
tkabila <tkabila@gmail.com>
2016-11-25 16:30:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,115 @@
-
-
- var submit = document.getElementById('submit_btn');
+function loadLoginForm(){
+    var loginHtml = `
+    <h3>Login/Register to unlock awesome features</h3>
+    <input type = "text" id = "username" placeholder = "username"/>
+    <input type = "password" id = "password"/>
+    <br/><br/>
+    <input type = "submit" id = "login_btn" value = "Login"/>
+    <input type = "submit" id = "register_btn" value = "Register"/>
+    `;
+    document.getElementById('login_area').innerHTML = loginHtml;
+ var submit = document.getElementById('login_btn');
  submit.onclick = function(){
      var request = new XMLHttpRequest();
    
     request.onreadystatechange = function(){
      if(request.readyState===XMLHttpRequest.DONE){
          if(request.status === 200){
-           alert('Logged  in successfully');
+          submit.value = 'Success!';
           }
        else if(request.status === 403){
-           alert('username/password is incorrect');
+           submit.value = 'Invalid credentials.Try again!!';
           }
         else if(request.status === 500){
            alert('Something went wrong on the server');
-          }
-     }     
+           submit.value = 'Login';
+         }
+         loadLogin();
+     }    
+     //
      };
      var username = document.getElementById('username').value;
      var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST','http://tkabila.imad.hasura-app.io/login', true);
+    request.open('POST','/login', true);
     request.setRequestHeader('Content-Type','application/json');             
     request.send(JSON.stringify({username:username,password:password}));
+    submit.value = 'Logging in... ';
       };
+       var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','/create-user', true);
+    request.setRequestHeader('Content-Type','application/json');             
+    request.send(JSON.stringify({username:username,password:password}));
+    register.value = 'Registering... ';
+      };
+}
+function loadloggedInUser(username){
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+     <h3>Hi <i>${username}</i></h3>
+     <a href = "/logout">Logout</a>
+    `;
+    }
+     function loadLogin(){
+      var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              var articles = document.getElementById('articles');
+              if (request.status === 200) {   
+                   if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
+}
+    function loadAticles(){
+      var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              var articles = document.getElementById('articles');
+              if (request.status === 200) {   
+                  var content = '<ul>';
+                  var articleData = JSON.parse(this.response.Text);
+                  for(var i = 0; i < articleData.length; i++){
+                      content +=`<li>
+                      <a href = "/articles/${articleData[i].title}">${articleData[i].heading}</a>}
+                      (${articleData[i].date.split('T')[0]})</li>`;
+                  }
+                  content += '<ul>';
+                  articles.innerHTML = content;
+              }else{
+                  articles.innerHTML('Oops! could not load all articles!');
+              }
+              }
+        
+    };
+    request.open('GET','/get-articles',true);
+    request.send(null);
+    }
+    loadLogin();
+    loadArticles();
