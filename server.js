dd301da8b08d876de13e3c3dd96d93d41dba7803
tkabila <tkabila@gmail.com>
2016-11-26 13:10:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,18 +121,19 @@ app.post('/create-user', function(req,res){
         
     });
 });
-app.get('/check-login',function(req,res){
-    if(req.session && req.session.auth && req.session.auth.userId){
-        pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) { res.status(500).send(err.toString());
-           }    else {
-                    res.send(result.rows[0].username);    
-                }
-        });
-     } else {
-            res.status(400).send('You are not logged in!');
-      }
-        
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 app.get('/logout', function (req, res) {
    delete req.session.auth;
