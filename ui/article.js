1cd4560e96cde97b0489be041a8bc620d971f6d6
tkabila <tkabila@gmail.com>
2016-11-26 15:04:17 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -10,74 +10,80 @@ function loadCommentForm () {
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
-var submit = document.getElementById('submit');
-submit.onclick = function(){
+  var submit = document.getElementById('submit');
+    submit.onclick = function () {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    document.getElementById('comment_text').value = '';
+                    loadComments();    
+                } else {
+                    alert('Error! Could not submit comment');
+                }
+                submit.value = 'Submit';
+          }
+        };
+        
+  var comment = document.getElementById('comment_text').value;
+        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({comment: comment}));  
+        submit.value = 'Submitting...';
+        
+    };
+}
+
+function loadLogin () {
+    // Check if the user is already logged in
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.Done){
-            if(request.status === 200){
-                document.getElementById('comment_text').value='';
-                loadcomments();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadCommentForm(this.responseText);
             }
-            else{
-                alert('Error! Could not submit comment');
-            }
-            submit.value = 'Submit';
         }
     };
-   var comment = document.getElementById('comment_text').value;
-   request.open('POST','/submit-comment/' + currentArticleTitle, true);
-   request.setRequestHeader('Content-Type','application/json');
-   request.send(JSON.stringify({comment: comment}));
-   submit.value = 'Submitting...';
-};
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
 }
-function loadLogin(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.Done){
-            if(request.status === 200){
-                loadCommentForm(this.responseText);
-        }
-        }
-};
-request.open('GET','/check-login',true);
-request.send(null);
-}
-function escapeHTML(text){
+
+function escapeHTML (text)
+{
     var $text = document.createTextNode(text);
     var $div = document.createElement('div');
     $div.appendChild($text);
     return $div.innerHTML;
-    }
-    function loadComments(){
-      var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.Done){
-            var comments = document.getElementById('comments'); 
-            if(request.status === 200){
+}
+
+function loadComments (){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var comments = document.getElementById('comments');
+            if (request.status === 200) {
                 var content = '';
                 var commentsData = JSON.parse(this.responseText);
-                for(var i=0; i<commentsData.length; i++){
+                for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
-                    content +=`
-                    <div class = "comment">
-                    <p> ${escapeHTML(commentsData[i].comment)}</p>
-                    <div class = "commenter">
-                    ${commentsData[i].username}-${time.toLocaleTimeString()} on ${time.toLocaleDateString()}
-                    </div>
-                    </div>
-                    `;
+                    content += `<div class="comment">
+                        <p>${escapeHTML(commentsData[i].comment)}</p>
+                        <div class="commenter">
+                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                        </div>
+                    </div>`;
                 }
                 comments.innerHTML = content;
             }   
-    else{
-        comments.innerHTML('Oops! Could not load comments!');
-    }
-    }
+   else {
+                comments.innerHTML('Oops! Could not load comments!');
+            }
+        }
     };
-    request.open('GET','/get-comments/' +currentArticleTitle,true);
+    
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
     request.send(null);
-    }
-    loadLogin();
-    loadComments();
+}
+loadLogin();
+loadComments();
