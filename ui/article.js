e8bc317d9a374297a646509b73a2723da26d8014
tkabila <tkabila@gmail.com>
2016-12-02 16:13:03 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -2,12 +2,13 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
     var commentFormHtml = `
+        <div>
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
         <br/>
         <input type="submit" id="submit" value="Submit" />
         <br/>
-        
+        </div>
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
     
