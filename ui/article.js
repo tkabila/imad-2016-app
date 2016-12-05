8002c7ef6c86595ef4fe4b6829101d873fa5d1e3
tkabila <tkabila@gmail.com>
2016-12-06 01:25:11 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -5,7 +5,7 @@ function loadCommentForm () {
         
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
+        <br/><br/>
         <input type="submit" id="submit" value="Submit" />
         <br/>
         
